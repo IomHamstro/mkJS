3e159eddc8035bd98358009054a47899cf59a629
Ольга <telejnikova.olga@gmail.com>
2014-04-09 13:24:49 +0400

    v.2

--- js/matrix.js
+++ js/matrix.js
@@ -1,44 +1,46 @@
-var c = document.getElementById("c");
+//Тег <canvas> предоставляет ограниченную область для рисования в ней.
+//    В нашем примере мы будем рисовать 2D объекты.
+//    При инициализации тег пуст и для рисования нам необходимо получить доступ к обрабатываемому контексту.
+var c = document.getElementById("c"); // получаем наш элемент канвас
 var ctx = c.getContext("2d");
 
-//making the canvas full screen
+//делаем на весь экран
 c.height = window.innerHeight;
 c.width = window.innerWidth;
 
-//chinese characters - taken from the unicode charset
-var chinese = "10 10 011 010 101010 1001 010 10 1010 10 10 10 1010  10 1010 10 ";
-//converting the string into an array of single characters
-chinese = chinese.split("");
+var symbols = "10 10 011 010 101010 1001 010 10 1010 10 10 10 1010  10 1010 10 ";
+//конвертируем строку в массив
+symbols = symbols.split("");
 
-var font_size = 10;
-var columns = c.width/font_size; //number of columns for the rain
-//an array of drops - one per column
-var drops = [];
+var font_size = 10; // здесь мы задаем размер наших символов
+var columns = c.width/font_size; //количество колонок для дождя
+
+var drops = []; //массив капель. по одному на каждую колонку
 //x below is the x coordinate
-//1 = y co-ordinate of the drop(same for every drop initially)
+//1 = y координата капли(то же самое для каждой капли изначально)
 for(var x = 0; x < columns; x++)
 drops[x] = 1;
 
-//drawing the characters
+//изображаем символы
 function draw()
     {
-        //Black BG for the canvas
-        //translucent BG to show trail
-        ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-        ctx.fillRect(0, 0, c.width, c.height);
+        //черный background для канвас
+        ctx.fillStyle = "rgba(0, 0, 0, 0.05)"; // Определяет цвет заливки.
+        ctx.fillRect(0, 0, c.width, c.height); //Заливает прямоугольную область цветом, определенный свойством fillStyle.
 
-        ctx.fillStyle = "#0F0"; //green text
+        ctx.fillStyle = "#0F0"; //зеленый текст
         ctx.font = font_size + "px arial";
-        //looping over drops
+        //цикл для каплей
         for(var i = 0; i < drops.length; i++)
         {
-        //a random chinese character to print
-        var text = chinese[Math.floor(Math.random()*chinese.length)];
+        //рандомный символ на печать
+        var text = symbols[Math.floor(Math.random()*symbols.length)];
         //x = i*font_size, y = value of drops[i]*font_size
-        ctx.fillText(text, i*font_size, drops[i]*font_size);
+        ctx.fillText(text, i*font_size, drops[i]*font_size); //Рисуем текст залитый цветом, определенным fillStyle.
 
         //sending the drop back to the top randomly after it has crossed the screen
         //adding a randomness to the reset to make the drops scattered on the Y axis
+        //отправка капли наверх случайно после того, как она пересекла экран
         if(drops[i]*font_size > c.height && Math.random() > 0.975)
         drops[i] = 0;
 
@@ -47,4 +49,4 @@ function draw()
         }
         }
 
-setInterval(draw, 33);
+setInterval(draw, 35); //функция для отложенного запуска кода постоянно с заданной периодичностью
